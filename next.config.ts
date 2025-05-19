bbb0f016dd52e290bafa76b2f0408ebd4ec89888
Misael Alves <112561288+misaelalves99@users.noreply.github.com>
2025-05-18 21:14:38 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["cdn.pixabay.com"],
-  },
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
